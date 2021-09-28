159162991d50d9792930756d6b10ddf4dea3f8fe
asqar orwell <54055815+assquire@users.noreply.github.com>
2021-09-28 15:15:23 +0600

    Add files via upload

---
+++ src/main/java/com/example/light_on/repository/CountryRepository.java
@@ -0,0 +1,15 @@
+package com.example.light_on.repository;
+
+import com.example.light_on.models.Country;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CountryRepository extends CrudRepository<Country,Long> {
+
+    List<Country> findAll();
+
+    Optional<Country> findByCode(String code);
+
+}

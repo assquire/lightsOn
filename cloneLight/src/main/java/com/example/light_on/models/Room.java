ac0f40aaac16a4d88d232d5484f40841420d577a
asqar orwell <54055815+assquire@users.noreply.github.com>
2021-09-28 14:43:56 +0600

    Add files via upload

---
+++ cloneLight/src/main/java/com/example/light_on/models/Room.java
@@ -0,0 +1,75 @@
+package com.example.light_on.models;
+
+import com.sun.istack.NotNull;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @NotNull
+    @Column(name = "light_status")
+    private Boolean lightStatus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public Boolean getLightStatus() {
+        return lightStatus;
+    }
+
+    public void setLightStatus(Boolean lightStatus) {
+        this.lightStatus = lightStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return Objects.equals(id, room.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ cloneLight/src/main/java/com/example/light_on/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.example.light_on.repository;
+
+import com.example.light_on.models.Room;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface RoomRepository extends CrudRepository<Room, Long> {
+    List<Room> findAll();
+}
